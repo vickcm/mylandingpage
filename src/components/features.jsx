cfb7383138fe8de15f72910cb81ecbbd41fd5b3e
vickcm <77595917+vickcm@users.noreply.github.com>
2024-06-24 21:37:12 -0300

    iconos feather
    css

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -10,7 +10,7 @@ export const Features = (props) => {
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
                   {' '}
-                  <i className={d.icon}></i>
+                  <i  data-feather={d.icon}></i>
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
                 </div>
